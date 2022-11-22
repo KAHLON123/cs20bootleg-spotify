cbe2338ca94fc50aacd0ee0fde22c7eccccf6e38
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-11-22 13:27:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // HTML ELEMENTS
 let output = document.getElementById('output');
+let toccata = document.getElementById('toccata-in-d');
+let playBtn = document.getElementById('play-btn');
+let pauseBtn = document.getElementById('pause-btn');
 
-let songs = [];
+
+let songs = [artist: ];
+
+// make a display for each array index individually and play a certain song (event listener on button)
+
+
+
+function play(songName) {
+    songName.play();
+}
+
+function pause(songName) {
+    songName.pause();
+}
