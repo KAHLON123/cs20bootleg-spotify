94c0b6b448cc96d5892c7ba646dc9c6aab343e0d
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-11-25 13:28:18 -0700

    Create library.js

---
+++ library.js
@@ -0,0 +1,16 @@
+// rando library
+
+// return random decimal b/t inclusive low + exclusive high
+function randomDec(low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+// return random integer b/t inclusive low + exclusive high
+function returnIn(low, high) {
+    return Math.floor(randomDec(low, high));
+}
+
+// return random rgb colour
+function randomRGB() {
+    return `rgb(${randomInt(0, 256)}, ${randomInt(0, 256)}, ${randomInt(0, 256)})`;
+}
