61925e4dbac8d81b645c9bd6fdb8be40e8cb0258
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-11-25 13:28:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // HTML ELEMENTS
 let output = document.getElementById('output');
-
-let toccata = document.getElementById('toccata-in-d');
-// let bolero = document.getElementById('ravel-bolero');
-
 let playBtn = document.getElementById('play-btn');
 let pauseBtn = document.getElementById('pause-btn');
+let menuEl = document.getElementById('song-menu');
 
+// SONG ELEMENTS
+let toccata = new Audio('audio/amyTurk.mp3');
+let bolero = new Audio('audio/mauriceRavelBolero.mp3');
 
-let songs = [{ artist: 'Bach', piece: 'Toccata', id: 'toccata' }];
+// ARRAY
+let songs = [{ artist: 'Bach', piece: 'Toccata', audioEl: toccata },
+{ artist: 'Ravel', piece: 'Bolero', audioEl: bolero }];
 //  { artist: 'Ravel' piece: 'Bolero' id: 'bolero' }];
 
 // make a display for each array index individually and play a certain song (event listener on button)
@@ -18,23 +20,32 @@ pauseBtn.addEventListener('click', pauseHandler);
 function playHandler() {
     console.log('play');
     let selection = menuEl.value;
-    if (selection === 'Toccata in d minor, Amy Turk') {
-        play(songs[0].id);
+    if (selection === 'Toccata') {
+        play(toccata);
+    } else if (selection === 'Bolero') {
+        play(bolero);
+    } else if (selection === 'Shuffle') {
+        play(songs[returnIn(0, songs.length)].audioEl);
+        console.log(returnIn(0, songs.length));
     }
 }
 
 function pauseHandler() {
     console.log('pause');
     let selection = menuEl.value;
-    if (selection === 'Toccata in d minor, Amy Turk') {
-        pause(songs[0].id);
+    if (selection === 'Toccata') {
+        pause(toccata);
+    } else if (selection === 'Bolero') {
+        pause(bolero);
+    } else if (selection === 'Shuffle') {
+        pause(songs[returnIn(0, songs.length)].audioEl);
     }
 }
 
-function play(songName) {
-    songName.play();
+function play(audioEl) {
+    audioEl.play();
 }
 
-function pause(songName) {
-    songName.pause();
+function pause(audioEl) {
+    audioEl.pause();
 }
