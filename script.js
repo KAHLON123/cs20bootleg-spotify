c3f25330357ddf50325765363a1ff3b4a6b6bbae
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-11-25 12:16:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 // HTML ELEMENTS
 let output = document.getElementById('output');
+
 let toccata = document.getElementById('toccata-in-d');
+// let bolero = document.getElementById('ravel-bolero');
+
 let playBtn = document.getElementById('play-btn');
 let pauseBtn = document.getElementById('pause-btn');
 
 
-let songs = [artist: ];
+let songs = [{ artist: 'Bach', piece: 'Toccata', id: 'toccata' }];
+//  { artist: 'Ravel' piece: 'Bolero' id: 'bolero' }];
 
 // make a display for each array index individually and play a certain song (event listener on button)
+playBtn.addEventListener('click', playHandler);
+pauseBtn.addEventListener('click', pauseHandler);
 
+function playHandler() {
+    console.log('play');
+    let selection = menuEl.value;
+    if (selection === 'Toccata in d minor, Amy Turk') {
+        play(songs[0].id);
+    }
+}
 
+function pauseHandler() {
+    console.log('pause');
+    let selection = menuEl.value;
+    if (selection === 'Toccata in d minor, Amy Turk') {
+        pause(songs[0].id);
+    }
+}
 
 function play(songName) {
     songName.play();
