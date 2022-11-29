84f138883490bb18f2d8457edff85ea266f01660
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-11-29 13:27:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // HTML ELEMENTS
 let output = document.getElementById('output');
+
 let playBtn = document.getElementById('play-btn');
 let pauseBtn = document.getElementById('pause-btn');
 let playlistBtn = document.getElementById('playlist-btn');
+let removeBtn = document.getElementById('remove-btn');
+
 let menuEl = document.getElementById('song-menu');
 let outputEl = document.getElementById('playlist-output');
 
@@ -11,8 +14,7 @@ let toccata = new Audio('audio/amyTurk.mp3');
 let bolero = new Audio('audio/mauriceRavelBolero.mp3');
 
 // ARRAYS
-let songs = [{ artist: 'Bach', piece: 'Toccata', audioEl: toccata },
-{ artist: 'Ravel', piece: 'Bolero', audioEl: bolero }];
+let songs = [{ artist: 'Bach', piece: 'Toccata', audioEl: toccata }, { artist: 'Ravel', piece: 'Bolero', audioEl: bolero }];
 
 let playlist = loadPlaylist();
 
@@ -20,33 +22,42 @@ let playlist = loadPlaylist();
 playBtn.addEventListener('click', playHandler);
 pauseBtn.addEventListener('click', pauseHandler);
 playlistBtn.addEventListener('click', addToPlaylist);
+removeBtn.addEventListener('click', removeSong);
 
+
+// DISPLAY PLAYLIST:
+listDisplay();
+function listDisplay() {
+    outputEl.innerHTML = '';
+    let outputStr = '';
+    for (let i = 0; i < playlist.length; i++) {
+        outputStr += getPlaylistHTMLStr(songs[i], i);
+    }
+    outputEl.innerHTML = outputStr;
+
+}
 
 //EVENT FUNCTIONS
 function playHandler() {
-    console.log('play');
     let selection = menuEl.value;
-    if (selection === 'Toccata') {
-        play(toccata);
-    } else if (selection === 'Bolero') {
-        play(bolero);
-    } else if (selection === 'Shuffle') {
+    if (selection === 'Shuffle') {
         play(songs[returnIn(0, songs.length)].audioEl);
         console.log(returnIn(0, songs.length));
-    }    //  else if (selection === 'Navarra') {
-    //     play
-    // }
+    } else {
+        play(songs[indexOf(selection, songs)].audioEl);
+    }
 }
 
 function pauseHandler() {
-    console.log('pause');
     let selection = menuEl.value;
     if (selection === 'Toccata') {
         pause(toccata);
     } else if (selection === 'Bolero') {
         pause(bolero);
     } else if (selection === 'Shuffle') {
-        pause(songs[returnIn(0, songs.length)].audioEl);
+        let randNum = returnIn(0, songs.length);
+        menuEl.value = songs[randNum].piece;
+        pause(songs[randNum].audioEl);
     }
 }
 
@@ -54,48 +65,43 @@ function addToPlaylist() {
     if (menuEl.value === 'Shuffle') {
         alert('Please choose a piece');
     } else {
-        var indexFound = indexOf(menuEl.value, songs);
+        var indexFound = indexOf(menuEl.value, playlist);
+        console.log(indexFound);
         if (indexFound === -1) {
             playlist.push(songs[indexFound]);
-            localStorage.setItem("playlist", JSON.stringify(playlist));
+            listSave();
+            listDisplay();
             console.log(playlist);
         } else {
             alert('That piece is already in that playlist');
         }
     }
-
-
 }
 
 function removeSong() {
-
+    if (menuEl.value === 'Shuffle') {
+        alert('Please choose a piece');
+    } else {
+        playlist.splice(indexOf(menuEl.value, playlist), 1);
+        listSave();
+        listDisplay();
+    }
 }
 
 // HELPER FUNCTIONS
-
 function indexOf(item, array) {
     for (i = 0; i < array.length; i++) {
         if (item === array[i].piece) {
             return i;
-            // piece exists in playlist
         }
     }
     return -1;
 }
 
-function displayPlaylist() {
-    outputEl.innerHTML = '';
-    let outputStr = '';
-    for (let i = 0; i < playlist.length; i++) {
-        outputStr += getPlaylistHTMLStr(playlist[i], i);
-    }
-    outputEl.innerHTML = outputStr;
-}
-
 function getPlaylistHTMLStr(song, indexNum) {
     return `
     <div>
-    ${indexNum}:
+    ${indexNum}:<br>
     ARTIST: ${song.artist}<br>
     TITLE: ${song.piece}
     `;
@@ -103,10 +109,16 @@ function getPlaylistHTMLStr(song, indexNum) {
 
 function play(audioEl) {
     audioEl.play();
+    console.log('play');
 }
 
 function pause(audioEl) {
     audioEl.pause();
+    console.log("pause");
+}
+
+function listSave() {
+    localStorage.setItem("playlist", JSON.stringify(playlist));
 }
 
 function loadPlaylist() {
